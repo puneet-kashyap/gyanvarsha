abab73ca268d00ad9cba35eaa45efc23c3f84603
puneet-kashyap <puneet.kashyap@loyalty.com>
2017-05-01 15:53:39 -0400

    added contextOut as arrays of object

--- webhook.js
+++ webhook.js
@@ -17,12 +17,12 @@ server.get('/', (req, res, next) => {
 server.post('/', (req, res, next) => {
 
    if (req.body.status === 200 && req.body.result.action === 'courseLevel'){
-        res.setHeader('content-type', 'application/json');
+        //res.setHeader('content-type', 'application/json');
         res.json({
             speech: "Hello speech from webhook",
             displayText: "Hello from webhook",
             data: {},
-            contextOut: [],
+            contextOut: [{}],
             source: "gyanvarsha-webhook"    
         });
     }
