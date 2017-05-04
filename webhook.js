734fd20022e5a08770a4a53424f2095e0829744c
puneet-kashyap <puneet.kashyap@loyalty.com>
2017-05-04 12:17:25 -0400

    Update course suggestions

--- webhook.js
+++ webhook.js
@@ -22,22 +22,22 @@ server.post('/', (req, res, next) => {
        switch(result.action){
         case 'courseLevel':
             if (result.parameters.EducationLevel == 'PostSecondary'){
-                responseText = `Certification or Diploma courses are best for ${result.parameters.EducationLevel} students. Would you like to do Certification, Diploma, or Degree program ?`;
+                responseText = `Degree or Diploma courses are best for ${result.parameters.EducationLevel} students. Would you like to do Certification, Diploma, or Degree program ?`;
             } else if (result.parameters.EducationLevel == 'Graduate'){
-                responseText = `Bachelor's or Master's degree courses are best for ${result.parameters.EducationLevel} students. Would you like to do Bachelor's or Master's degree ?`;
+                responseText = `Master's degree courses are best for ${result.parameters.EducationLevel} students. Would you like to do Bachelor's or Master's degree ?`;
             } else if (result.parameters.EducationLevel == 'PostGraduate'){
                 responseText = `Master's degree or specialized Certification are best for ${result.parameters.EducationLevel} students. Would you like to do Master's or certification ?`;
             } else if (result.parameters.EducationLevel == 'Diploma'){
                 responseText = `Bachelor's Degree or Certification courses are best for ${result.parameters.EducationLevel} students. Would you like to do Degree or Certification program ?`;
             } else {
-                responseText = `Diploma or Certification courses are very popular. Would you like to do Diploma or Certification program ?`;
+                responseText = `Certification courses are very popular. Would you like to do Diploma or Certification program ?`;
             }
             break;
         case 'offerProgram':
             responseText = `That's great. ${result.parameters.EducationLevel} is a very good choice. In which field are you interested in? e.g. Accounting, Management, Engineering, MBA etc.`;
             break;
         case 'offerCourses':
-            responseText = `Awesome. ${result.parameters.Courses} has a good scope. Please contact Anil from Gyanvarsha at Phone # +91 9210214031.`;
+            responseText = `Awesome. ${result.parameters.Courses} has a good scope. Please contact Anil from Gyanvarsha at Phone # +91 9210214031 for further assistance.`;
             break;
         default: 
             responseText = "Oops, something went wrong";
