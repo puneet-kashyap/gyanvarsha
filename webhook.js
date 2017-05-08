e328f1ae10d49cca358bac88db59a01dd803ab44
puneet-kashyap <puneet.kashyap@loyalty.com>
2017-05-08 09:09:30 -0400

    function extracted from post request

--- webhook.js
+++ webhook.js
@@ -3,6 +3,25 @@ const Restify = require('restify');
 const server = Restify.createServer({
   name: "GyanvarshaBot"
 });
+
+var MongoClient = require('mongodb').MongoClient
+  , assert = require('assert');
+
+// Connection URL
+var url = 'mongodb://chatbot:chatbot@ds133331.mlab.com:33331/gyanvarsha';
+
+// Use connect method to connect to the server
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  var cursor = db.collection('users_info').find( );
+  cursor.each(function(err,doc){
+      console.log(doc);
+  })
+  console.log("Connected successfully to MongoDb server");
+
+  db.close();
+});
+
 const request = require('request');
 const PORT = process.env.PORT || 3001;
 
